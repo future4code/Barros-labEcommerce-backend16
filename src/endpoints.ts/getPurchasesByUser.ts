76c3894ba4bb5823dc0ce3fd6cd76b3e0b78ecd1
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-11-29 18:41:51 -0300

    Adicionar endpoint de buscar compras do usuário

---
+++ src/endpoints.ts/getPurchasesByUser.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { connection } from "../data/connection";
+
+export const getPurchasesByUser = async (req: Request, res: Response): Promise<void> => {
+    const userId = req.params.user_id as string
+    let errorCode = 422
+
+    try {
+
+        if (!userId) {
+            errorCode = 422
+            throw new Error("ID de usuário não informado.");
+        }
+
+        const getUser = await connection("labecommerce_users")
+        .where('id', userId)
+
+        if (getUser.length < 0) {
+            errorCode = 422
+            throw new Error("ID de usuário não encontrado.");
+        }
+
+        const result = await connection.select("labecommerce_users.name as user_name", "labecommerce_products.name as product_name", "quantity", "total_price")
+        .from("labecommerce_purchases")
+        .join("labecommerce_users", 'user_id', '=', 'labecommerce_users.id')
+        .join('labecommerce_products', 'product_id', '=', 'labecommerce_products.id')
+        .where('user_id', userId)
+  
+        res.status(200).send(result)
+
+    } catch (error: any) {
+        res.status(500).send(error.message)
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import { createProduct } from "./endpoints.ts/createProduct";
 import { createUser } from "./endpoints.ts/createUser";
 import { getAllProducts } from "./endpoints.ts/getAllProducts";
 import { getAllUsers } from "./endpoints.ts/getAllUsers";
+import { getPurchasesByUser } from "./endpoints.ts/getPurchasesByUser";
 
 app.post("/users", createUser)
 app.get("/users", getAllUsers)
 app.post("/products", createProduct)
 app.get("/products", getAllProducts)
 app.post("/purchases", addPurchase)
+app.get("/users/:user_id/purchases", getPurchasesByUser)
