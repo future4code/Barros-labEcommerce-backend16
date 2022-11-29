aad62b1ad39a709b40eccc2d3cda1b578bc4b723
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-11-29 16:57:57 -0300

    Adicionar requisição getAllProducts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import app from "./app";
 import { createProduct } from "./endpoints.ts/createProduct";
 import { createUser } from "./endpoints.ts/createUser";
+import { getAllProducts } from "./endpoints.ts/getAllProducts";
 import { getAllUsers } from "./endpoints.ts/getAllUsers";
 
 app.post("/users", createUser)
 app.get("/users", getAllUsers)
 app.post("/products", createProduct)
+app.get("/products", getAllProducts)
