54593d340fa95da59066ff767399533833550feb
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-11-29 16:55:58 -0300

    Adicionar endpoint createProducts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import app from "./app";
+import { createProduct } from "./endpoints.ts/createProduct";
 import { createUser } from "./endpoints.ts/createUser";
 import { getAllUsers } from "./endpoints.ts/getAllUsers";
 
 app.post("/users", createUser)
 app.get("/users", getAllUsers)
+app.post("/products", createProduct)
